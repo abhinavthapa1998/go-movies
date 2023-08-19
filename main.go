592132bbe74c84768d8b1ae41e4c0f7dd3b57386
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-08-19 23:39:44 +0530

    getMovie added

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -26,6 +28,14 @@ var movies []Movie
 func getMovies(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type","application/json")
 	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request){
